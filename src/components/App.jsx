ae1971cd71a3b31f2e7c26965dcc1edf501a6575
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2023-11-14 17:32:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import { Container } from './Container/Container.styled';
 import { Section } from './Section/Section';
